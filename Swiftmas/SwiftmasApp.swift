d95a4387c27aec8f2b00261069f519c17ea9a947
Alessio Garzia Marotta Brusco <alessiobrusco609@gmail.com>
2024-12-11 17:07:43 +0100

    Initial commit.

---
+++ Swiftmas/SwiftmasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftmasApp.swift
+//  Swiftmas
+//
+//  Created by Alessio Garzia Marotta Brusco on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftmasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
